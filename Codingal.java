c15ce8aa4693381e90f622d94e406bd41e20d935
CSLearner19 <K2309961@students.katyisd.org>
2026-06-06 11:33:20 -0500

    Add files via upload

---
+++ Codingal.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args){
+    String first = "Cod";
+    String second = "ingal";
+    String codingal = first+second;
+    String codingalTrick="Welcome to Codingal";
+    String codingalCapital=codingal.toUpperCase();
+    String codingalSmall=codingalCapital.toLowerCase();
+    int lengthOfCodingal=codingal.length();
+    int lengthOfCodngalTrick=codingalTrick.length();
+    int sum=lengthOfCodingal+lengthOfCodngalTrick;
+    System.out.println(codingal);
+    System.out.println(codingalTrick);
+    System.out.println(codingalCapital);
+    System.out.println(codingalSmall);
+    System.out.println(sum);
+}
+    
+}
