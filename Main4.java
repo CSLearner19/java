9cec31fb25a847645e488f3b5757d7a7396848fc
CSLearner19 <K2309961@students.katyisd.org>
2026-06-10 11:00:18 -0500

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,10 @@
+class Main4 {
+    int age;
+    Main4(int age){
+        this.age=age;
+    }
+    public static void main(String[] args) {
+        Main4 obj = new Main4(24);
+        System.out.println("obj.age: " + obj.age);
+    }
+}
