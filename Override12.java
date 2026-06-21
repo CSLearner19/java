e09ae7e9d1a60f2c805a06651438d2934aa7adfa
CSLearner19 <K2309961@students.katyisd.org>
2026-06-21 08:25:29 -0500

    Add files via upload

---
+++ Override12.java
@@ -0,0 +1,17 @@
+class Parent{
+    public void sayHello(){
+        System.out.println("Hello from Parent");
+    }
+}
+class Child extends Parent{
+    @Override
+    public void sayHello(){
+        System.out.println("Hello from Child");
+    }
+}
+class Override12{
+    public static void main(String[] args) {
+        Parent p = new Child();
+        p.sayHello();
+    }
+}
